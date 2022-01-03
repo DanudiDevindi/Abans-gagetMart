8d2bd8d72c7968885a01a2cb26c5d2716ac9e5ca
Danudi <danusilva1997@gmail.com>
2022-01-03 16:14:22 +0530

    initial commit

---
+++ src/main/java/com/bolton/abans_gagetmart/AbansGagetmartApplication.java
@@ -0,0 +1,13 @@
+package com.bolton.abans_gagetmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbansGagetmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbansGagetmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolton/abans_gagetmart/AbansGagetmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolton.abans_gagetmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbansGagetmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
